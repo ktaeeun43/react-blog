43e654da4637273d9f9807e1a4183741bee38e7d
ktaeeun43 <ktaeeun43@gmail.com>
2022-10-11 14:56:12 +0900

    db 연결

---
+++ api/controllers/auth.js
@@ -0,0 +1,35 @@
+import { db } from "../db.js"
+import bcrypt from "bcryptjs";
+
+export const register = (req, res) => {
+
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+    db.query(q, [req.body.email, req.body.name], (err, data) => {
+        if(err) return res.json(err);
+        if (data.length) return res.status(409).json("User already exists!");
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+        const values = [
+            req.body.username,
+            req.body.email,
+            hash,
+        ]
+
+        console.log(values , "밸류");
+        db.query(q,[values], (err,data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json("User has been created.");
+        })
+    }) 
+}
+
+export const login = (req, res) => {
+    
+}
+
+export const logout = (req, res) => {
+    
+}
